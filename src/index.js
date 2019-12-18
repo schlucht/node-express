a590e5a5221cafaa0da32a91da8b25607be371df
schlucht <lothi@schmidschlucht.ch>
2019-12-18 07:33:17 +0100

    Start Projekt

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const errorController = require("./controllers/errors");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+//create a server object:
+
+app.use(errorController.get404);
+
+app.listen(8080); //the server object listens on port 8080
---
+++ src/models/product.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const path = require("path");
+const rootDir = require("../util/path");
+const products = [];
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+  save() {
+    const p = path.join(rootDir, "data", "products.json");
+    console.log(rootDir);
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+  static fetchAll() {
+    const p = path.join(rootDir, "data", "products.json");
+    fs.readFile(p, (err, fileContent) => {
+      if (!err) {
+        return [];
+      }
+    });
+    return products;
+  }
+};
